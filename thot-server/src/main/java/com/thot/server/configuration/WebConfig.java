e9298b028e7d608b1bd111806b92966e0e32afd5
ggalazzo <gabor.galazzo@gmail.com>
2023-02-15 16:13:24 +0100

    Internal barcode/qrcode generator

--- thot-server/src/main/java/com/thot/server/configuration/WebConfig.java
+++ thot-server/src/main/java/com/thot/server/configuration/WebConfig.java
@@ -1,5 +1,10 @@
 package com.thot.server.configuration;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,5 +20,17 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addMapping("/**")
       .allowedOrigins("*")
       .allowedMethods("*");
+  }
+
+  @Bean
+  public ConfigurableServletWebServerFactory webServerFactory() {
+    TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+    factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+      @Override
+      public void customize(Connector connector) {
+        connector.setProperty("relaxedQueryChars", "|{}[]");
+      }
+    });
+    return factory;
   }
 }
---
+++ thot-server/src/main/java/com/thot/server/controller/UtilsController.java
@@ -0,0 +1,59 @@
+package com.thot.server.controller;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.Code128Writer;
+import com.google.zxing.oned.EAN13Writer;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/utils")
+public class UtilsController {
+
+    @GetMapping(value = "/barcode", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> renderBarcode(
+            @RequestParam String code,
+            @RequestParam(defaultValue = "300") int width,
+            @RequestParam(defaultValue = "150") int height) throws IOException {
+
+        Code128Writer barcodeWriter = new Code128Writer();
+        BitMatrix bitMatrix = barcodeWriter.encode(code.replace("{","").replace("}",""),
+                BarcodeFormat.CODE_128, width, height);
+
+        var img =  MatrixToImageWriter.toBufferedImage(bitMatrix);
+        var baos = new ByteArrayOutputStream();
+        ImageIO.write(img, "jpeg", baos);;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE);
+        return ResponseEntity.ok().headers(httpHeaders).body(baos.toByteArray());
+    }
+
+    @GetMapping(value = "/qrcode", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> renderQrcode(@RequestParam String code,
+            @RequestParam(defaultValue = "300") int size) throws WriterException, IOException {
+
+        QRCodeWriter  barcodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = barcodeWriter.encode(code, BarcodeFormat.QR_CODE, size, size);
+
+        var img =  MatrixToImageWriter.toBufferedImage(bitMatrix);
+        var baos = new ByteArrayOutputStream();
+        ImageIO.write(img, "jpeg", baos);;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE);
+        return ResponseEntity.ok().headers(httpHeaders).body(baos.toByteArray());
+    }
+}
