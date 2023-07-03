16ea570fe95be6c54f6a0903118bdd59823741a1
ggalazzo <gabor.galazzo@gmail.com>
2023-07-03 23:36:31 +0200

    added cors

--- thoth-server/src/main/java/com/thoth/server/configuration/security/SecurityConfig.java
+++ thoth-server/src/main/java/com/thoth/server/configuration/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
+        http.cors().and().csrf()
                 .disable()
                 .authorizeRequests()
                 .anyRequest().permitAll()
