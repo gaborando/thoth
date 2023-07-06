9f69fdf8078b237d82051755b4ec39db1bc5232a
ggalazzo <gabor.galazzo@gmail.com>
2023-07-06 08:54:47 +0200

    ionic-angular update

--- thoth-web/src/app/guards/authentication.guard.ts
+++ thoth-web/src/app/guards/authentication.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import {environment} from "../../environments/environment";
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationGuard implements CanActivate {
+export class AuthenticationGuard  {
 
 
   constructor() {
