576140863a409f2a3136ac4dd43c58b8fbc8f40f
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-11-13 14:02:10 +0100

    Correct exception handling for unauthorized

--- thoth-server/src/main/java/com/thoth/server/controller/CustomExceptionHandler.java
+++ thoth-server/src/main/java/com/thoth/server/controller/CustomExceptionHandler.java
@@ -2,6 +2,7 @@ package com.thoth.server.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -15,8 +16,13 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
         ex.printStackTrace();
-        ErrorResponse error = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage());
-        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        if(ex instanceof AccessDeniedException){
+            ErrorResponse error = new ErrorResponse(HttpStatus.UNAUTHORIZED, ex.getLocalizedMessage());
+            return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+        }else {
+            ErrorResponse error = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage());
+            return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     private record ErrorResponse(HttpStatus status, String message) {
