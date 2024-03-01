3b1d5c02cbc07516fc43f837fb73cf4e09a6bb91
Gabor Galazzo <gabor.galazzo@gmail.com>
2024-03-01 17:25:20 +0100

    Optimize SVG to JPEG conversion process
    The code now utilizes a singleton for Playwright instantiation to improve performance and resource efficiency. A new browser page is generated for each conversion request, enhancing concurrency handling. Plus, adjustments on RabbitMQ timeouts and concurrency level would result in the improvement of the application's performance.

--- thoth-common/src/main/java/org/thoth/common/Svg2Jpeg.java
+++ thoth-common/src/main/java/org/thoth/common/Svg2Jpeg.java
@@ -16,45 +16,64 @@ import java.util.logging.Logger;
 
 public class Svg2Jpeg {
 
-    public static byte[] convert(String svg, long delay) {
-        for(int i = 0; i<3; i++) {
+    private static Playwright playwright = Playwright.create();
+    private static Browser browser = playwright.chromium().launch();
+
+    private static synchronized Page getPage() {
+        try {
+            return browser.newPage();
+        }catch (Exception e){
             try {
-                try (Playwright playwright = Playwright.create()) {
-                    try (Browser browser = playwright.chromium().launch()) {
-                        try (var page = browser.newPage()) {
-                            page.setContent(svg);
-                            page.evaluate("""
-                                    () => {
-                                        var scale = 2;
-                                        var svg = document.getElementsByTagName('svg')[0];
-                                        var iWidth = svg.width.baseVal.value;
-                                        var iHeight = svg.height.baseVal.value;
-                                        svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
-                                        svg.removeAttribute('width');
-                                        svg.removeAttribute('height');
-                                        svg.style.setProperty('margin', 0, 'important');
-                                        svg.style.setProperty('border', 0, 'important');
-                                        svg.style.setProperty('padding', 0, 'important');
-                                        svg.style.setProperty('position', 'fixed', 'important');
-                                        svg.style.setProperty('left', 0, 'important');
-                                        svg.style.setProperty('top', 0, 'important');
-                                        
-                                        svg.style.setProperty('width', (iWidth * scale) + 'px', 'important');
-                                        svg.style.setProperty('height', (iHeight * scale) + 'px',  'important');
-                                    }
-                                    """);
-                            Thread.sleep(delay);
-                            return page.locator("svg").screenshot(new Locator.ScreenshotOptions()
-                                    .setType(ScreenshotType.JPEG)
-                                    .setQuality(100));
-                        }
-                    }
+                browser.close();
+            }catch (Exception ignored){}
+            try {
+                playwright.close();
+            } catch (Exception ignored) {
+            }
+            playwright = Playwright.create();
+            browser = playwright.chromium().launch();
+            System.gc();
+            return browser.newPage();
+        }
+    }
+
+
+    public static byte[] convert(String svg, long delay) {
+        for (int i = 0; i < 3; i++) {
+            try {
+                try (var page = getPage()) {
+                    page.setContent(svg);
+                    page.evaluate("""
+                            () => {
+                                var scale = 2;
+                                var svg = document.getElementsByTagName('svg')[0];
+                                var iWidth = svg.width.baseVal.value;
+                                var iHeight = svg.height.baseVal.value;
+                                svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
+                                svg.removeAttribute('width');
+                                svg.removeAttribute('height');
+                                svg.style.setProperty('margin', 0, 'important');
+                                svg.style.setProperty('border', 0, 'important');
+                                svg.style.setProperty('padding', 0, 'important');
+                                svg.style.setProperty('position', 'fixed', 'important');
+                                svg.style.setProperty('left', 0, 'important');
+                                svg.style.setProperty('top', 0, 'important');
+                                
+                                svg.style.setProperty('width', (iWidth * scale) + 'px', 'important');
+                                svg.style.setProperty('height', (iHeight * scale) + 'px',  'important');
+                            }
+                            """);
+                    Thread.sleep(delay);
+                    return page.locator("svg").screenshot(new Locator.ScreenshotOptions()
+                            .setType(ScreenshotType.JPEG)
+                            .setQuality(100));
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
-                System.out.println("Document Generation Failed. Attempt " + (i+1));
+                System.out.println("Document Generation Failed. Attempt " + (i + 1));
             }
         }
         throw new RuntimeException("Document Generation Failed after 3 attempts");
     }
+
 }
