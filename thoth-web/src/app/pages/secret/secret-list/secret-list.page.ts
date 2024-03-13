527206d548ee51c04711227bcfb07894c20f735e
Gabor Galazzo <gabor.galazzo@gmail.com>
2024-03-13 12:10:44 +0100

    Secret Service in Angular and includes corresponding ListPage for displaying and managing secrets. The service provides CRUD operations for secrets, while the added page uses this service to enable user interactions like creating, viewing, and deleting secrets.

---
+++ thoth-web/src/app/pages/secret/secret-list/secret-list.page.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {ScreenMessageService} from "../../../services/screen-message.service";
+import {ApiKeyService} from "../../../services/api/api-key.service";
+import {AlertController, IonInput, IonModal, ToastController} from "@ionic/angular";
+import {ApiKey} from "../../../common/types/api-key";
+import {ListPage} from "../../../common/utils/ui-patterns/list-page";
+import {Secret} from "../../../common/types/secret";
+import {SecretService} from "../../../services/api/secret.service";
+
+@Component({
+  selector: 'app-secret-list',
+  templateUrl: './secret-list.page.html',
+  styleUrls: ['./secret-list.page.scss'],
+})
+export class SecretListPage extends ListPage<Secret> implements OnInit {
+
+
+  constructor(
+    private screenMessageService: ScreenMessageService,
+    private secretService: SecretService
+  ) {
+    super(secretService)
+  }
+
+
+  ngOnInit() {
+  }
+
+  crateSecret(modal: IonModal, name: any,
+              value: any) {
+    return this.screenMessageService.loadingWrapper(async () => {
+      const key = await this.secretService.create(name,
+        value);
+      this.elements?.push(key);
+      return modal.dismiss();
+    }, true)
+  }
+
+  delete(secret: Secret) {
+    return this.screenMessageService.showDeleteAlert(async () => {
+      await this.secretService.delete(secret.name);
+      this.elements = this.elements?.filter(s => s.name !== secret.name);
+    })
+  }
+
+
+}
---
+++ thoth-web/src/app/services/api/secret.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import {Page} from "../../common/utils/fetchUtils";
+import {ApiKey} from "../../common/types/api-key";
+import {environment} from "../../../environments/environment";
+import {AuthenticatedService} from "./authenticated.service";
+import {DataFetcher} from "../../common/utils/service-patterns/data-fetcher";
+import {Secret} from "../../common/types/secret";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SecretService  extends AuthenticatedService implements DataFetcher<Secret>{
+
+  constructor() {
+    super()
+  }
+
+  async findAll(page = 0, filter = '', signal: AbortSignal): Promise<Page<Secret>>{
+    return fetch((await environment()).apiUrl + '/secret/?page=' + page, {
+      method: 'GET',
+      headers: this.getHeaders(),
+      signal
+    }).then(async r => {
+      if (!r.ok) {
+        throw await r.json()
+      }
+      return await r.json()
+    });
+  }
+
+  async create(name: string,
+               value: string): Promise<Secret>{
+    return fetch((await environment()).apiUrl + '/secret/', {
+      method: 'POST',
+      headers: this.postHeaders(),
+      body: JSON.stringify({
+        name,
+        value
+      })
+    }).then(async r => {
+      if (!r.ok) {
+        throw await r.json()
+      }
+      return await r.json()
+    });
+  }
+
+  async delete(id: string): Promise<any>{
+    return fetch((await environment()).apiUrl + '/secret/' + id, {
+      method: 'DELETE',
+      headers: this.getHeaders(),
+    }).then(async r => {
+      if (!r.ok) {
+        throw await r.json()
+      }
+      return await r.text()
+    });
+  }
+}
